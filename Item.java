7d3dfe861b6752a966d791ff0fe802cf695edea4
Madison Sarmiento <59521537+mady-dx@users.noreply.github.com>
2021-10-08 09:30:54 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,10 +1,21 @@
 package orderSystem;
 
 public class Item {
-int itemID;
+String itemID;
 String itemName;
 boolean isAvailable;
 double price;
 String pictureID;
 int quantity;
+  
+  //Constructor, takes in 6 parameters; don't need to overload constructor
+  public Item(String itemID, String itemName, boolean isAvailable, double price, String pictureID, int quantity){
+    this.itemID = itemID;
+    this.itemName = itemName;
+    this.isAvailable = isAvailable;
+    this.price = price;
+    this.pictureID = pictureID;
+    this.quantity = quantity;
+  }
+  
 }

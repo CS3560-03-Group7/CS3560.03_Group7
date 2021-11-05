2f812a256896bd8022f0fe3e07a841ca983c80c9
Madison Sarmiento <59521537+mady-dx@users.noreply.github.com>
2021-11-05 15:13:47 -0700

    Update Main.java
    fixed issue of pictures not working;
    need to figure out css and connecting it to labels and such; problem for tomorrow

--- Main.java
+++ Main.java
@@ -3,9 +3,12 @@ package com.example.ordertime;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
@@ -31,21 +34,81 @@ public class Main extends Application {
     private Label mShakeLabel = new Label("Milk Shake");
     private Label rbFloatLabel = new Label("Rootbeer Float");
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) {
+
+        //for the nuggies; set to be eventually placed in a gridpane or something for future
         Button btnAddToOrder = new Button("Add to Order");
         btnAddToOrder.setPrefWidth(200);
-        VBox vNugs = new VBox(nuggies, nugLabel, btnAddToOrder);
+        nuggies.setFitWidth(150);
+        nuggies.setFitHeight(150);
+        nugLabel.getStyleClass().add(".label-white");
+        VBox vNugs = new VBox(nuggies, nugLabel);
+        vNugs.setAlignment(Pos.CENTER);
         vNugs.setSpacing(15);
 
-        Scene scene = new Scene(vNugs);
+        //for the pretty patties ~~ i mean borgs
+        burger.setFitWidth(150);
+        burger.setFitHeight(150);
+        burgLabel.getStyleClass().add(".label-white");
+        VBox vBurg = new VBox(burger, burgLabel);
+        vBurg.setAlignment(Pos.CENTER);
+        vBurg.setSpacing(15);
+
+        //for the fritas
+        fries.setFitWidth(150);
+        fries.setFitHeight(150);
+        fryLabel.getStyleClass().add(".label-white");
+        VBox vFry = new VBox(fries, fryLabel);
+        vFry.setAlignment(Pos.CENTER);
+        vFry.setSpacing(15);
+
+        //for the lunchables
+        kidsMeal.setFitWidth(150);
+        kidsMeal.setFitHeight(150);
+        kMealLabel.getStyleClass().add(".label-white");
+        VBox vKMeal = new VBox(kidsMeal, kMealLabel);
+        vKMeal.setAlignment(Pos.CENTER);
+        vKMeal.setSpacing(15);
+
+        //for the boys in my yard
+        milkShake.setFitWidth(150);
+        milkShake.setFitHeight(150);
+        mShakeLabel.getStyleClass().add(".label-white");
+        VBox vShake = new VBox(milkShake, mShakeLabel);
+        vShake.setAlignment(Pos.CENTER);
+        vShake.setSpacing(15);
+
+        //for the rootbeer floats
+        rootbeerFloat.setFitWidth(150);
+        rootbeerFloat.setFitHeight(150);
+        rbFloatLabel.getStyleClass().add(".label-white");
+        VBox vFloat = new VBox(rootbeerFloat, rbFloatLabel);
+        vFloat.setAlignment(Pos.CENTER);
+        vFloat.setSpacing(15);
+
+        //for all your menu needs
+        GridPane menu = new GridPane();
+        menu.setAlignment(Pos.CENTER);
+        menu.setHgap(5);
+        menu.setVgap(5);
+        menu.add(vNugs, 0, 0);
+        menu.add(vBurg,1,0);
+        menu.add(vFry, 2, 0);
+        menu.add(vKMeal,0,1);
+        menu.add(vShake,1,1);
+        menu.add(vFloat,2,1);
+
+        Scene scene = new Scene(menu);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
         primaryStage.setTitle("Menu");
+        primaryStage.setResizable(true);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }

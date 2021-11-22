c551ca5e3904a26ae370a3fcf98a5f5e6c4f8b3c
jjones307 <82834147+jjones307@users.noreply.github.com>
2021-11-22 14:06:15 -0800

    Add files via upload

---
+++ Java SQL Connector/SQLConnector.java
@@ -0,0 +1,105 @@
+package dbConnector;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetFactory;
+import javax.sql.rowset.RowSetProvider;
+
+public class SQLConnector  {
+	private String dbURL;
+    private String user;
+    private String pass;
+	
+    public SQLConnector(String url, String username, String password) {
+    	this.dbURL = url;
+    	this.user = username;
+    	this.pass = password;
+    }
+    
+	private Connection connect() {
+		Connection conn = null;
+		
+		
+        try {
+			conn = DriverManager.getConnection(dbURL, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        
+        return conn;
+	}
+	
+	public CachedRowSet query(String query) throws SQLException{
+		Connection conn = connect();
+		ResultSet rs = null;
+		
+		
+		Statement stmt = conn.createStatement();
+		rs = stmt.executeQuery(query);		
+		
+		RowSetFactory factory = RowSetProvider.newFactory();
+		CachedRowSet rowset = factory.createCachedRowSet();
+		rowset.populate(rs);
+		if(rs != null) 
+            try {rs.close();} catch (SQLException e){e.printStackTrace();}
+        if(stmt!= null) 
+            try {stmt.close();} catch (SQLException e){e.printStackTrace();}
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        
+		return rowset;
+	}
+	
+	public int update(String query) throws SQLException{
+		Connection conn = connect();		
+		int rowsUpdated = 0;
+		
+		Statement stmt = conn.createStatement();
+		rowsUpdated = stmt.executeUpdate(query);
+		
+		 if(stmt!= null) 
+	            try {stmt.close();} catch (SQLException e){e.printStackTrace();}
+	        try {
+	            if (conn != null && !conn.isClosed()) {
+	                conn.close();
+	                }
+	            } catch (SQLException ex) {
+	                ex.printStackTrace();
+	            }
+	        
+			return rowsUpdated;
+	}
+	
+	public void getServerInfo()  throws SQLException {
+		Connection conn = connect();
+		
+		if (conn != null) {
+            DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+            System.out.println("Database name: " + conn.getCatalog());
+            System.out.println("Driver name: " + dm.getDriverName());
+            System.out.println("Driver version: " + dm.getDriverVersion());
+            System.out.println("Product name: " + dm.getDatabaseProductName());
+            System.out.println("Product version: " + dm.getDatabaseProductVersion());
+        }
+		
+		try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+	}
+
+}

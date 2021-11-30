991ce7ba0136532f24c886c34369d6bcee7f54f5
nightbrds <92063569+nightbrds@users.noreply.github.com>
2021-11-29 23:37:20 -0800

    Add files via upload
    order class V1.0 :) basically just constructs an array list to attach to the cart's arraylist and also instantiates a tracking number that will be set by Cart

--- Order.java
+++ Order.java
@@ -1,5 +1,29 @@
-package orderSystem;
+package com.order.main;
 
-public class Order {
+public class Order
+{
+    private int trackingNum; //this tracking number will be set by the Cart class
+    private ArrayList<OrderItem> orderList = new ArrayList<OrderItem>(); //this arraylist will be attached to the cart to track the order
 
+    public Order()
+    {
+        trackingNum = 0;
+    }
+
+    //standard accessor/mutator methods for trackingNum
+    public int getTrackingNum()
+    {
+        return trackingNum;
+    }
+
+    public void setTrackingNum(int trackingNum)
+    {
+        this.trackingNum = trackingNum;
+    }
+
+    //setter that will attach the cart and all of its items to the order
+    public void setOrderList(ArrayList<OrderItem> cartList)
+    {
+        orderList = cartList;
+    }
 }

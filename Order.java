6926166d54282a286c25d474c9f806c87411e91e
dmrco <92063569+dmrco@users.noreply.github.com>
2021-11-30 14:12:24 -0800

    Update Order.java
    Order Class V1.1

--- Order.java
+++ Order.java
@@ -1,13 +1,35 @@
 package com.order.main;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.rowset.CachedRowSet;
+
 public class Order
 {
+    private int orderID;
+    private int orderCartID;
     private int trackingNum; //this tracking number will be set by the Cart class
     private ArrayList<OrderItem> orderList = new ArrayList<OrderItem>(); //this arraylist will be attached to the cart to track the order
+    private LocalDateTime orderTime;
 
-    public Order()
+    public Order(SQLConnector s, int cartID, int trackingNum, LocalDateTime orderTime)
     {
-        trackingNum = 0;
+        this.orderCartID = cartID;
+        this.trackingNum = trackingNum;
+        this.orderTime = orderTime;
+        
+        String query = "SELECT orderID FROM `order` WHERE orderCartID = " + Integer.toString(this.orderCartID) + ";";
+        try {
+            CachedRowSet results = s.query(query);
+            results.next();
+            this.orderID = results.getInt("orderID");
+        } catch (SQLException ex) {
+            Logger.getLogger(Order.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
     //standard accessor/mutator methods for trackingNum
@@ -25,5 +47,9 @@ public class Order
     public void setOrderList(ArrayList<OrderItem> cartList)
     {
         orderList = cartList;
+    }
+    
+    public void sendOrderToKitchen(){
+        System.out.println("Order #" + this.trackingNum + " sent to kitchen.");
     }
 }
